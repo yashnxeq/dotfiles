0f3cb6f7ade51a980b9b8ca0ebf2e95fc84a078e
Yashnxeq <yashtajne@proton.me>
2025-10-21 10:49:02 +0530

    Windows + NixOS config

--- nvim/lua/plugins/buffers.lua
+++ nvim/lua/plugins/buffers.lua
@@ -1,35 +1,45 @@
 
 
--- Bufferline Tabs switch
+-- Bufferline Tabs switch (Barbar)
 for i = 1, 9 do
   vim.keymap.set("n", ("<leader>%s"):format(i), function()
     local ft = vim.bo.filetype
     if ft ~= "neo-tree" then
-      vim.cmd("BufferLineGoToBuffer " .. i)
+      vim.cmd("BufferGoto " .. i)
     end
   end, { desc = "Go to buffer " .. i })
 end
 
-
 return {
-    {
-      "akinsho/bufferline.nvim",
-      version = "*",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      opts = {
-        options = {
-          mode = "buffers",
-          always_show_bufferline = true,
-          diagnostics = "nvim_lsp",
-          separator_style = "slant",
-          offsets = {
-            {
-              filetype = "neo-tree",
-              highlight = "Directory",
-              separator = true,
-            }
-          }
-        }
-      }
-    }
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {},
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      animation = true,
+      auto_hide = false,
+      tabpages = false,
+      clickable = true,
+      icons = {
+        buffer_index = false,
+        buffer_number = false,
+        diagnostics = {
+          [vim.diagnostic.severity.ERROR] = { enabled = true, icon = " " },
+          [vim.diagnostic.severity.WARN]  = { enabled = true, icon = " " },
+          [vim.diagnostic.severity.INFO]  = { enabled = true, icon = " " },
+          [vim.diagnostic.severity.HINT]  = { enabled = true, icon = " " },
+        },
+        separator = { left = "", right = "" },
+        separator_at_end = true,
+      },
+      insert_at_end = false,
+      maximum_padding = 2,
+      minimum_padding = 1,
+      sidebar_filetypes = {
+        ["neo-tree"] = { event = "BufWipeout" },
+      },
+    },
+  },
 }
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,9 +1,17 @@
 
 return {
-  "yorumicolors/yorumi.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme yorumi")
-  end,
+  -- Kanagawa colorscheme
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        local is_windows = vim.fn.has("win32") == 1
+        if is_windows then
+            vim.cmd("colorscheme kanagawa-lotus")
+        else
+            vim.cmd("colorscheme kanagawa-dragon")
+        end
+    end,
+  },
 }
